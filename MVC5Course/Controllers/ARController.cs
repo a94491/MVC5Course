c979936c394165a42839911f4f08c5c7cee01e27
MVC5 <a94491@mail.com>
2018-07-11 14:18:07 +0800

    day2

---
+++ MVC5Course/Controllers/ARController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ARController : Controller
+    {
+        // GET: AR
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ViewTest()
+        {
+            string model = "MY Data";
+            return View((object) model);
+        }
+        public ActionResult PartialViewTest()
+        {
+            string model = "MY Data";
+            return PartialView("ViewTest",(object)model);
+        }
+        public ActionResult FileTest(string dl)
+        {
+            if (String.IsNullOrEmpty(dl))
+            {
+                return File(Server.MapPath("~/App_Data/123.png"),
+                    "image/png");
+            }
+            else
+            {
+                return File(Server.MapPath("~/App_Data/123.png"),
+                    "image/png", "@@@.png");
+            }
+        }
+
+    }
+}
---
+++ MVC5Course/Controllers/ClientsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class ClientsController : BaseController
+    {
+        
+        ClientRepository repo;
+        OccupationRepository occuRepo;
+        public ClientsController()
+        {
+            //用repository取代 private FabricsEntities db = new FabricsEntities();
+            repo = RepositoryHelper.GetClientRepository();
+            occuRepo = RepositoryHelper.GetOccupationRepository(repo.UnitOfWork);
+        }
+            // GET: Clients
+        public ActionResult Index()
+        {
+            var client = repo.All();
+            return View(client.Take(10));
+        }
+        
+        public ActionResult Search(string keyword)
+        {
+            {
+                var client = repo.搜尋名稱(keyword);
+                         
+                return View("Index",client);
+            }
+
+           
+        }
+
+        // GET: Clients/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = repo.Find(id.Value);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+        // GET: Clients/Create
+        public ActionResult Create()
+        {
+            
+            ViewBag.OccupationId = new SelectList(occuRepo.All(), "OccupationId", "OccupationName");
+            return View();
+        }
+
+        // POST: Clients/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes,IdNumber")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.Add(client);
+                repo.UnitOfWork.Commit();
+                return RedirectToAction("Index");
+            }
+           
+            ViewBag.OccupationId = new SelectList(occuRepo.All(), "OccupationId", "OccupationName", client.OccupationId);
+            return View(client);
+        }
+
+        // GET: Clients/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = repo.Find(id.Value);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+         
+            ViewBag.OccupationId = new SelectList(occuRepo.All(), "OccupationId", "OccupationName", client.OccupationId);
+            return View(client);
+        }
+
+        // POST: Clients/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = repo.UnitOfWork.Context;
+                db.Entry(client).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           
+            ViewBag.OccupationId = new SelectList(occuRepo.All(), "OccupationId", "OccupationName", client.OccupationId);
+            return View(client);
+        }
+       
+        // GET: Clients/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = repo.Find(id.Value);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+        // POST: Clients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Client client = repo.Find(id);
+            repo.Delete(client);
+            repo.UnitOfWork.Commit();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                repo.UnitOfWork.Context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
